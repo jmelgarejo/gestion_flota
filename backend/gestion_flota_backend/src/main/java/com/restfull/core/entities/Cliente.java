a9c1ff2dded53912fff436e1d919aa0317b0e958
Juan Andres Melgarejo <juan.andres@melgarejo.cl>
2023-06-27 00:37:06 -0400

    correcciones para crud

--- backend/gestion_flota_backend/src/main/java/com/restfull/core/entities/Cliente.java
+++ backend/gestion_flota_backend/src/main/java/com/restfull/core/entities/Cliente.java
@@ -1,6 +1,17 @@
 package com.restfull.core.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long idCliente;
     private int personaId;
     private String razonSocial;
