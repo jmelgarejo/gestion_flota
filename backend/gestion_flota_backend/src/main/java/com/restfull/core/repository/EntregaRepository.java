3d8c687a48e4b47f9fb05993a1a8c2047848e2ef
Eddie <evasquez@sisdef.cl>
2023-06-26 22:49:38 -0400

    By Eddie

---
+++ backend/gestion_flota_backend/src/main/java/com/restfull/core/repository/EntregaRepository.java
@@ -0,0 +1,50 @@
+package com.restfull.core.repository;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.restfull.core.entities.Entrega;
+
+
+
+@Repository
+public class EntregaRepository {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+
+	public List<Entrega> getAllEntregaEstado() {
+		DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String sql = "SELECT id, cliente, estado, create_at from entregas where estado='PENDIENTE';";
+		List<Entrega> entregaList = new ArrayList<Entrega>();
+		List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+		rows.forEach(row -> {
+			Entrega entrega = new Entrega();
+			Long id = ((Number)row.get("id")).longValue();
+			entrega.setId((long)id.intValue());
+			entrega.setCliente((String)row.get("cliente"));
+			//entrega.setCreateAT((Date)row.get("create_at"));
+			entrega.setEstado((String)row.get("estado"));
+			try {
+				LocalDateTime fecha = (LocalDateTime)row.get("create_at");
+				String strBaja = fecha.format(CUSTOM_FORMATTER);
+				Date fechaNew =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strBaja);
+				entrega.setCreateAT(fechaNew);
+			}catch(Exception e) {
+				entrega.setCreateAT(null);
+			}		
+			entregaList.add(entrega);
+		});
+		return entregaList;
+}
+}
